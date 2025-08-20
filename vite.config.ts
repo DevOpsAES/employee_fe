fe6a9e6cfba1c366a5f1e876242adbececa9374e
DevOpsAES <kader.khan@anwargroup.com>
2025-08-20 23:38:38 +0600

    final changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,24 @@
-import { defineConfig } from 'vite';
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  const env = loadEnv(mode, process.cwd());
+  
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/employeebe': {
+          target: 'http://localhost:5000',
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  };
 });
